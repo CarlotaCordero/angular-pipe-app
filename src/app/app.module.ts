5367f99c64d55ffa99d680591789faae1007fffb
CarlotaCordero <carlota.cdb221@gmail.com>
2024-12-19 09:46:34 +0100

    Languaje Setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -6,7 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
 
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 @NgModule({
   declarations: [
@@ -18,7 +23,9 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule,
 ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
